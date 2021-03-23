9afd8e19884c92cb56f68184d2ba248ea86c4d28
t-tamaru <t-tamaru@sakura.ad.jp>
2021-03-23 10:05:06 +0900

    不要な空白行削除

--- functions.php
+++ functions.php
@@ -161,7 +161,6 @@ function rino_matsushima_scripts()
     if (is_singular() && comments_open() && get_option('thread_comments')) {
         wp_enqueue_script('comment-reply');
     }
-
 }
 add_action('wp_enqueue_scripts', 'rino_matsushima_scripts');
 
