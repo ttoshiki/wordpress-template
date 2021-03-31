f868a7234862e3d07167c14f394b2ecb8dd2b70e
t-tamaru <t-tamaru@sakura.ad.jp>
2021-03-31 22:58:15 +0900

    gulpfile更新

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,7 @@
 // gulpプラグインの読み込み
 const gulp = require("gulp");
 // Sassをコンパイルするプラグインの読み込み
-const sass = require("gulp-sass");
-const cleanCSS = require("gulp-clean-css");
-const rename   = require("gulp-rename");
+const sass = require("gulp-dart-sass");
 sass.compiler = require('sass');
 
 const imagemin = require('gulp-imagemin');
@@ -12,26 +10,22 @@ const pngquant = require('imagemin-pngquant');
 const changed = require('gulp-changed');
 
 gulp.task("imagemin", function () {
-  return gulp.watch(
-    './assets/images/**',
-    function() {
-        return gulp
-            .src('./assets/images/**')
-            .pipe(changed('./dist/assets/images/**'))
-            .pipe(
-                imagemin([
-                    pngquant({
-                        quality: [.60, .70], // 画質
-                        speed: 1 // スピード
-                    }),
-                    mozjpeg({ quality: 65 }), // 画質
-                    imagemin.svgo(),
-                    imagemin.optipng(),
-                    imagemin.gifsicle({ optimizationLevel: 3 }) // 圧縮率
-                ])
-            )
-            .pipe(gulp.dest('./dist/assets/images'))
-        });
+    return gulp
+        .src('./assets/images/**')
+        .pipe(changed('./dist/images/**'))
+        .pipe(
+            imagemin([
+                pngquant({
+                    quality: [.60, .70], // 画質
+                    speed: 1 // スピード
+                }),
+                mozjpeg({ quality: 65 }), // 画質
+                imagemin.svgo(),
+                imagemin.optipng(),
+                imagemin.gifsicle({ optimizationLevel: 3 }) // 圧縮率
+            ])
+        )
+        .pipe(gulp.dest('./dist/images'))
     });
 
 gulp.task("watch-scss", function() {
@@ -43,23 +37,15 @@ gulp.task("watch-scss", function() {
                 gulp
                     .src("./assets/scss/*.scss")
                     // Sassのコンパイルを実行
-                    .pipe(
-                        sass({
-                            outputStyle: "expanded"
-                        })
-                            // Sassのコンパイルエラーを表示
-                            // (これがないと自動的に止まってしまう)
-                            .on("error", sass.logError)
-                    )
-                    // cssフォルダー以下に保存
-                    .pipe(gulp.dest("./"))
-                    .pipe(cleanCSS())
-                    .pipe(rename({
-                        extname: '.min.css'
-                    }))
+                    .pipe(sass.sync().on('error', sass.logError))
                     .pipe(gulp.dest('./'))
+                    // cssフォルダー以下に保存
             );
         })
 })
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./assets/scss/*.scss', ['sass']);
+});
 
 gulp.task('default', gulp.parallel('watch-scss', 'imagemin'));
